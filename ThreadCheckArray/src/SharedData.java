12b6c00848826a224696e059fb4b15aed77c8ce1
sandra26 <sandra.knizhnik@e.braude.ac.il>
2022-11-02 11:24:22 +0200

    .

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
 /**
- *.
- * Class responsible for abaling sharing data via an ArrayList
+ * Class responsible for abaling sharing data using an ArrayList
  * @author user
  */
 public class SharedData 
@@ -11,37 +10,60 @@ public class SharedData
 	private boolean flag;
 	private final int b;
 	
+	/**
+	 * constractor that creates a list of integers and declares a parameter
+	 * @param array
+	 * @param b
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b; // number to look for
 	}
 
+	/**
+	 * @return an array
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
-
+	
+	/**
+	 * @param winArray, void method dosent return anything , sets boolean array  
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**
+	 * @return ArrayList of integers
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/**
+	 * @return the int b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return a boolean value that represented as flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/**
+	 * @param flag sets value of flag 
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
--- ThreadCheckArray/src/ThreadCheckArray.java
+++ ThreadCheckArray/src/ThreadCheckArray.java
@@ -2,11 +2,10 @@
 import java.util.ArrayList;
 
 /**
- * 
- * @author user
+ * @author user 
+ * class of runneble that checks the array workes with threds
  *
  */
-
 public class ThreadCheckArray implements Runnable 
 {
 	private boolean flag;
@@ -15,6 +14,10 @@ public class ThreadCheckArray implements Runnable
 	ArrayList<Integer> array;
 	int b;
 	
+	/**
+	 * @param sd
+	 * constractor that gets the array and accespts data from class
+	 */
 	public ThreadCheckArray(SharedData sd) 
 	{
 		this.sd = sd;	
@@ -23,9 +26,14 @@ public class ThreadCheckArray implements Runnable
 			array = sd.getArray();
 			b = sd.getB();
 		}		
-		winArray = new boolean[array.size()];
+		winArray = new boolean[array.size()]; //gets size of array 
 	}
 	
+	/**
+	 * @param n
+	 * @param b
+	 * gets flag and integer and using threads runs it and syncronizes the data
+	 */
 	void rec(int n, int b)
 	{
 		synchronized (sd) 
@@ -35,7 +43,7 @@ public class ThreadCheckArray implements Runnable
 		}	
 		if (n == 1)
 		{
-			if(b == 0 || b == array.get(n-1))
+			if(b == 0 || b == array.get(array.size()-n-1))
 			{
 				flag = true;
 				synchronized (sd) 
@@ -43,7 +51,7 @@ public class ThreadCheckArray implements Runnable
 					sd.setFlag(true);
 				}			
 			}
-			if (b == array.get(n-1))
+			if (b == array.get(array.size()-n-1))
 				winArray[n-1] = true;
 			return;
 		}
@@ -59,10 +67,13 @@ public class ThreadCheckArray implements Runnable
 		rec(n-1, b);
 	}
 
+	/**
+	 *void method dosent return a thing uses rec method in order to use the threds and sync actions
+	 */
 	public void run() {
 		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.size() -1, b - array.indexOf(array.size()- 1));
+				rec(array.size()-1, b - array.get(array.size()-1));
 			else 
 				rec(array.size()-1, b);
 		if (array.size() == 1)
